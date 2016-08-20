7c6b5a17d1a7cb9671d5888bcbafb9cabdbf3ef9
Wacław Łabuda <waclab1807@gmail.com>
2016-08-20 16:24:57 +0200

    Add downloading file module

--- app/components/old_script.js
+++ app/components/old_script.js
@@ -1,38 +1,10 @@
-/**
- * Created by waclabdev on 15.08.16.
- */
+var app = angular.module('firstApp', []);
 
-app.controller('mainController', ['$scope', function($scope){
+app.controller('ExampleController', ['$scope','$window', function ($scope, $window) {
 
-    $scope.msg = 'mainController';
-
-    var canvas = window._canvas = new fabric.Canvas('canvas');
-
-    /** downloading picture */
-    $scope.downloadCanvas = function () {
-
-        var canvas = document.getElementById("canvas");
-        var image = canvas.toDataURL();
-        var aLink = document.createElement('a');
-        console.log(aLink);
-        var evt = document.createEvent("HTMLEvents");
-
-        try
-        {
-            evt.initEvent("click");
-            aLink.download = 'image.png';
-            aLink.href = image;
-            aLink.dispatchEvent(evt);
-        }
-        catch (ex)
-        {
-            evt.initEvent("click", true, false);
-            aLink.download = 'image.png';
-            aLink.href = image;
-            aLink.dispatchEvent(evt);
-        }
-
-    };
+    angular.element($window).bind('resize', function() {
+        console.log('kkkk');
+    });
 
     $scope.customConf = {
         spacing: 50,
@@ -41,6 +13,65 @@ app.controller('mainController', ['$scope', function($scope){
         colorCutOut: 'white',
         height: 0,
         width: 0
+    };
+
+    var canvas = window._canvas = new fabric.Canvas('canvas');
+
+
+    $scope.downloadCanvas = function () {
+
+        var test = {
+            konik: 'wow',
+            mysz: 15
+        };
+
+        var canvas = document.getElementById("canvas");
+        var image = canvas.toDataURL();
+        var aLink = document.createElement('a');
+        console.log('###########',aLink);
+        var evt = document.createEvent("HTMLEvents");
+
+        try
+        {
+            evt.initEvent("click");
+            aLink.download = 'image.jpg';
+            aLink.href = image;
+            aLink.dispatchEvent(evt);
+            console.log('Chrom like you rly');
+
+        }
+        catch (ex)
+        {
+            evt.initEvent("click", true, false);
+            aLink.download = 'image.jpg';
+            aLink.href = image;
+            aLink.dispatchEvent(evt);
+
+            console.log('IE dont like you rly dode');
+        }
+
+
+
+    };
+
+    fabric.Image.fromURL('img.jpg', function (img) {
+        canvas.add(img);
+        canvas.setHeight(img.getHeight());
+        canvas.setWidth(img.getWidth());
+        $scope.customConf.height = img.getHeight();
+        $scope.customConf.width = img.getWidth();
+    });
+
+
+
+    $scope.optionsCutOut = function (ofsetVertical, ofsetHorizontal) {
+        var options = {
+            top: ofsetVertical,
+            left: ofsetHorizontal,
+            fill: $scope.customConf.colorCutOut
+        };
+        options.radius = $scope.customConf.diameter;
+        canvas.add(new fabric.Circle(options));
     };
 
     $scope.create = function () {
@@ -82,49 +113,25 @@ app.controller('mainController', ['$scope', function($scope){
         }
     };
 
-    $scope.optionsCutOut = function (ofsetVertical, ofsetHorizontal) {
-        var options = {
-            top: ofsetVertical,
-            left: ofsetHorizontal,
-            fill: $scope.customConf.colorCutOut
-        };
-        options.radius = $scope.customConf.diameter;
-        canvas.add(new fabric.Circle(options));
+    $scope.downloadLink = function () {
+
+        fabric.log('jpeg dataURL with low quality: ', canvas.toDataURL({
+            format: 'jpg',
+            quality: 1
+        }));
+
+        fabric.log('jpeg dataURL with low quality: ', canvas.toDataURL({
+            format: 'png',
+            left: 100,
+            top: 100,
+            width: 200,
+            height: 200
+        }));
+
+        fabric.log('jpeg dataURL with low quality: ', canvas.toDataURL({
+            format: 'png',
+            multiplier: 2
+        }));
     };
 
-    $scope.uploader={};
-
-    /** uploading picture */
-    $scope.imageStrings = [];
-    $scope.processFiles = function(files){
-        angular.forEach(files, function(flowFile, i){
-            var fileReader = new FileReader();
-            fileReader.onload = function (event) {
-                var uri = event.target.result;
-                $scope.imageStrings[i] = uri;
-            };
-            fileReader.readAsDataURL(flowFile.file);
-        });
-    };
-
-    $scope.test = function() {
-        console.log($scope.uploader.flow); //todo rozebrac to drzewo
-        var canvas = document.getElementById("canvas");
-        var canvas2 = document.getElementById("canvas2");
-        var ctx = canvas.getContext("2d");
-
-        var image = new Image();
-        image.onload = function() {
-            ctx.drawImage(image, 0, 0, canvas2.width, canvas2.height);
-        };
-        image.src = $scope.imageStrings[0];
-        //fabric.Image.fromURL('image', function (img) {
-        //    canvas.add(img);
-        //    canvas.setHeight(img.getHeight());
-        //    canvas.setWidth(img.getWidth());
-        //    $scope.customConf.height = img.getHeight();
-        //    $scope.customConf.width = img.getWidth();
-        //});
-    };
-
-}] );
+}]);
